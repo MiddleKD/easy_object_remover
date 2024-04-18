7230a74aee6da7dc73fc29dea2013d3388146bf4
ml-favorfit-develop <122414946+MiddleKD@users.noreply.github.com>
2024-04-18 16:18:43 +0900

    update:auto text writer

--- app.py
+++ app.py
@@ -6,20 +6,22 @@ import gradio as gr
 from inference import load_lama_remover, load_kandinsky_remover, inferene
 from Favorfit_OCR.inference import call_easyocr
 from Favorfit_OCR.inference import inference as ocr_inference
+from auto_text_writer import main as auto_text_write
 
 lama_remover = load_lama_remover(
     model_path="/home/mlfavorfit/Desktop/lib_link/favorfit/kjg/0_model_weights/lama_inpainting/big-lama.pt",
     device="cuda"
 )
-    
-kandinsky_remover = load_kandinsky_remover(
-    model_path="/home/mlfavorfit/Desktop/lib_link/favorfit/kjg/0_model_weights/diffusion/Kandinsky/kandinsky-decoder-inpainting",
-    image_processor_path="/home/mlfavorfit/Desktop/lib_link/favorfit/kjg/0_model_weights/diffusion/Kandinsky/kandinsky-prior/image_processor",
-    image_encoder_path="/home/mlfavorfit/Desktop/lib_link/favorfit/kjg/0_model_weights/diffusion/Kandinsky/kandinsky-prior/image_encoder",
-    device="cuda"
-)
 
-ocr_model = call_easyocr(["ko", "en"])
+kandinsky_remover = None   
+# kandinsky_remover = load_kandinsky_remover(
+#     model_path="/home/mlfavorfit/Desktop/lib_link/favorfit/kjg/0_model_weights/diffusion/Kandinsky/kandinsky-decoder-inpainting",
+#     image_processor_path="/home/mlfavorfit/Desktop/lib_link/favorfit/kjg/0_model_weights/diffusion/Kandinsky/kandinsky-prior/image_processor",
+#     image_encoder_path="/home/mlfavorfit/Desktop/lib_link/favorfit/kjg/0_model_weights/diffusion/Kandinsky/kandinsky-prior/image_encoder",
+#     device="cuda"
+# )
+
+ocr_model = call_easyocr(["ko"])
 
 st_width, st_height = (None, None)
 
@@ -52,9 +54,9 @@ def run_inference(edited_image, is_kandinsky, resize_size):
     mask = edited_image["layers"][0]
     st_width, st_height = image.size
 
-    if all(cur >= resize_size for cur in image.size):
-        image = resize_store_ratio(image, resize_size)
-        mask = resize_store_ratio(mask, resize_size)
+    # if all(cur >= resize_size for cur in image.size):
+    #     image = resize_store_ratio(image, resize_size)
+    #     mask = resize_store_ratio(mask, resize_size)
 
     if is_kandinsky == True:
        output = inferene(image, mask, kandinsky_remover)
@@ -66,19 +68,37 @@ def run_inference(edited_image, is_kandinsky, resize_size):
 def run_ocr(image, ocr_mask_rank):
     img = image["background"].convert("RGB")
 
-    ocr_result, mask_image = ocr_inference(img, ocr_model, conf_threshold=0.3, mask_rank=ocr_mask_rank)
+    ocr_result, mask_image = ocr_inference(
+        img, 
+        ocr_model, 
+        conf_threshold=0.1, 
+        mask_rank=ocr_mask_rank if ocr_mask_rank != -1 else None)
+
     rgba_mask = mask_image.convert("RGB")
     rgba_mask.putalpha(mask_image)
 
     for cur in ((None, None), ({"background":img, "layers":[rgba_mask], "composite":img}, ocr_result)):
         yield cur
-        time.sleep(1.0)
+        time.sleep(5.0)
 
 def output_to_input(image):
     image = image.resize((st_width, st_height))
     for cur in (None, image):
         yield cur
-        time.sleep(0.5)
+        time.sleep(5.0)
+
+def run_auto_text_writer(image, ocr_mask_rank, resize_size):
+    image = image["background"].convert("RGB")
+
+    # if all(cur >= resize_size for cur in image.size):
+    #     image = resize_store_ratio(image, resize_size)
+    
+    new_image = auto_text_write(
+        image,
+        ocr_model=ocr_model,
+        remover_model=lama_remover,
+        mask_rank = ocr_mask_rank if ocr_mask_rank != -1 else None)
+    return new_image
 
 
 js_func = """
@@ -92,7 +112,7 @@ function refresh() {
 }
 """
 
-with gr.Blocks(js=js_func) as demo:
+with gr.Blocks(js=js_func, title="이미지 번역기") as demo:
     with gr.Row():
         with gr.Column():
             input_image = gr.ImageEditor(
@@ -107,12 +127,12 @@ with gr.Blocks(js=js_func) as demo:
                 show_download_button=False,
                 sources=("upload"),
             )
-            ocr_mask_rank = gr.Number(value=1,
+            ocr_mask_rank = gr.Number(value=-1,
                                       precision=0,
                                       label="Number of mask",
-                                      minimum=1,
+                                      minimum=-1,
                                       maximum=20,
-                                      info="텍스트 마스크 개수")
+                                      info="텍스트 마스크 개수, -1 지정시 모든 텍스트")
             ocr_btn = gr.Button(value="Get text mask", variant="secondary")
             ocr_text_box = gr.TextArea(label="Text from image")
         
@@ -129,13 +149,16 @@ with gr.Blocks(js=js_func) as demo:
                                         label="resize size",
                                         minimum=256,
                                         maximum=1024, 
-                                        info="이미지의 크기가 값보다 크면, 작은 면을 기준으로 크기를 조정합니다.")
-            is_kandinsky = gr.Checkbox(value=False, label="assume hidden space")
-            run_btn = gr.Button(variant="primary")
+                                        info="이미지의 크기가 값보다 크면, 작은 면을 기준으로 크기를 조정합니다.",
+                                        visible=False)
+            is_kandinsky = gr.Checkbox(value=False, label="assume hidden space", visible=False)
+            run_btn = gr.Button(value="Remove object", variant="primary")
+            auto_text_write_btn = gr.Button(value="Translate image", variant="primary")
             to_input = gr.Button(value="Pass to input", variant="secondary")
 
     ocr_btn.click(run_ocr, [input_image, ocr_mask_rank], [input_image, ocr_text_box])
     run_btn.click(run_inference, [input_image, is_kandinsky, resize_size_box], output_image)
+    auto_text_write_btn.click(run_auto_text_writer, [input_image, ocr_mask_rank, resize_size_box], output_image)
     to_input.click(output_to_input, [output_image], input_image)
 
 demo.launch(share=True)
---
+++ auto_text_writer.py
@@ -0,0 +1,155 @@
+import numpy as np
+import cv2
+from Favorfit_OCR.inference import inference as ocr_inference
+from inference import inferene as remover_inference
+from PIL import Image, ImageDraw, ImageFont
+from claude_chatbot.api import query_to_claude
+
+
+def get_mask_range(img: Image.Image):
+
+    # 그레이스케일로 변환
+    img = np.array(img.convert("RGB"))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 이진화 처리
+    thresh = cv2.inRange(gray, 255, 255)
+
+    # 윤곽선 찾기
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # 가장 큰 윤곽선 찾기
+    largest_area = 0
+    largest_contour = None
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > largest_area:
+            largest_area = area
+            largest_contour = cnt
+
+    # 가장 큰 영역의 바운딩 박스 구하기
+    x, y, w, h = cv2.boundingRect(largest_contour)
+
+    return x, y, w, h
+
+from colorthief import ColorThief
+def extract_text_color(original_image, mask_image):
+    original_image = np.array(original_image.convert("RGB"))
+    
+    original_image = original_image.copy()
+    x, y, w, h = get_mask_range(mask_image)
+    text_region = original_image[y:y+h, x:x+w]
+    text_image = Image.fromarray(text_region)
+    colors = ColorThief(text_image).get_palette(2)
+    return colors[1]
+
+def add_text_to_image(image, mask_image, text, font_path, text_color=(255,255,255)):
+    
+    font_size = 10
+    font = ImageFont.truetype(font_path, size=font_size)
+
+    x0,y0,x1,y1 = font.getbbox(text)
+    x, y, w, h = get_mask_range(mask_image)
+
+    while (x1-x0 < w*0.95) and (y1-y0 < h*0.95):
+        font_size += 1
+        font = ImageFont.truetype(font_path, font_size)
+        x0,y0,x1,y1 = font.getbbox(text)
+
+    draw = ImageDraw.Draw(image)
+    draw.text((x+w/2,y+h/2), text, font=font, fill=text_color, anchor="mm")
+    
+    return image
+
+def remove_object(image, mask_image, remover_model):
+    return remover_inference(image, mask_image, remover_model).resize(image.size)
+
+en_to_ko_system_prompt = """너는 초월 번역가야. string의 리스트를 한글로 번역하되 불가피할 경우 영어를 사용해도 좋아. 원본 텍스트의 글자 수와 최대한 글자 수가 비슷한 단어를 선택해. 
+        예를 들어, 'watch'의 경우 '시계' 보다는 '손목시계'를 선택해. 길이가 좀 더 맞으니까.
+        브랜드 마케팅에 사용될 거야, 상품 가치가 높은 단어를 사용해
+        이건 나의 커리어에 굉장히 중요한 일이야.
+        
+        규칙:
+        1. 원본 텍스트의 길이와 최대한 길이가 비슷한 단어를 선택
+        2. 1번 규칙을 지킬 것
+        3. 번역할 수 없더라도 최대한 예측할 것
+        
+        에시:
+        입력: ["WATCH", "NEW GENERATION", "smart"]
+        출력: ["손목시계", "새로운 세대의 등장", "똑똑한"]
+
+        입력: ['SHOP ALL', 'G-SHOCK']
+        출력: ["모두 구매", "지--샥"]
+
+        입력: ["LA CREME", "CHANEL", "CHANEL"]
+        출력: ["라 크레메", "샤-넬", "샤-넬]"""
+
+ko_to_en_system_prompt = """
+Provide the English translations of the given Korean marketing phrases. You must answer in ENGLISH!
+Each translation should be separated by a newline character (\n).
+The translations should be concise, high-impact English equivalents that capture the essence and tone of the original Korean text.
+The character count of the English translations should closely match the length of the Korean phrases, within 1-2 additional characters.
+Avoid using any special characters or punctuation, and apply appropriate capitalization (title case for single words, title case for the first word and lowercase for the rest in 2-3 word phrases, unless the phrase includes a proper noun).
+Provide only the requested output. No additional explanations are necessary.
+
+Input:
+["손목시계", "새로운 세대의 등장", "똑똑한"]
+Output:
+Wristwatch\nNew generation\nSmart
+
+Input:
+["모두 구매", "지--샥"] 
+Output:
+Shop all\nG-series
+
+Input:
+["라 크레메", "샤-넬", "샤-넬"]
+Output:
+La creme\nChanel\nChanel
+
+Input:
+["최고의 선물", "이상적인 선택", "프리미엄 품질"]
+Output:
+Premier gift\nIdeal choice\nPremium quality
+
+Input:
+["럭셔리 경험", "최신 트렌드", "혁신적 디자인"]
+Output:
+Luxury experience\nLatest trend\nInnovative design
+"""
+
+def translate_text(text_list):
+    response_text = query_to_claude(
+        system_prompt=ko_to_en_system_prompt,
+        max_tokens=50,
+        temperature=0,
+        message_contexts=[{
+                "role": "user",
+                "content": str(text_list),
+            }]
+    )
+
+    return response_text.content[0].text.split('\n')
+
+def main(image, ocr_model, remover_model, mask_rank=None, font_path="./Roboto-Medium.ttf"):
+
+    ocr_results, mask_images = ocr_inference(image, ocr_model, conf_threshold=0.1, mask_rank=mask_rank, return_mask_list=True)
+    cleaned_image = image
+    for _, mask_image in zip(ocr_results, mask_images):
+        cleaned_image = remove_object(cleaned_image, mask_image, remover_model)
+
+    translated_texts = translate_text(ocr_results)
+
+    new_image = cleaned_image
+    for text, mask_image in zip(translated_texts, mask_images):
+        try:
+            text_color = extract_text_color(image, mask_image)
+            new_image = add_text_to_image(
+                new_image, 
+                mask_image, 
+                text, 
+                font_path, 
+                text_color=text_color)
+        except Exception as e:
+            print(Warning(e))
+    return new_image
